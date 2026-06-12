9951e41938652f401db375bf2566a152815e07a5
ptaybron14 <ptaybron14@gmail.com>
2026-06-12 14:19:00 -0400

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -1,11 +1,14 @@
 # Compound interest calculator
+
 print("Compound Interest Calculator")
 
+# Get user input
 principal = float(input("Enter the principal: "))
 interest = float(input("Enter the estimated interest rate in decimal form: "))
 compound_years = int(input("Enter amount of compound years: "))
 
-#calculation
+# Calculate future value
 final_amount = principal * (1 + interest) ** compound_years
 
+# Display result
 print("Final amount: $" + str(round(final_amount, 2)))
